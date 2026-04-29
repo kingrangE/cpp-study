9be750df57fb697a0a877b6831fcad3c94b7469d
kingrangE <kilwon.jeon00@gmail.com>
2026-04-30 06:29:07 +0900

    chores: 주석 추가 (test code 안내 주석 추가)

--- MyStringTest/main.cpp
+++ MyStringTest/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "my_string.h"
 
+
+
 int main(){
+    // 현재 unit test C++ 코드 작성 중 대체 예정
     MyString mystr("aab");
     
     mystr+="aba";
-    mystr.printContent();
-    std::cout << mystr.inStr("baba")<<std::endl;
+    mystr.printlnStr();
+    std::cout << mystr.in("baba")<<std::endl;
     std::cout << (mystr<"aaaba") <<std::endl;
     std::cout << (mystr>"aaaba")<<std::endl;
     std::cout << (mystr=="aaaba")<<std::endl;
